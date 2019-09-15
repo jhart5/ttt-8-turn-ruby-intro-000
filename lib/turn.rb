c3d88f6e1f91c3a4100ef10bd007ab01a59e86c8
josh hart <joshua.hart@protonmail.com>
2019-09-15 17:16:34 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input = input.to_i
+  index = input - 1
+  return index
+end
+
+def position_taken?(board, index)
+  !(((board[index] == " ")||(board[index == ""]))||(board[index] == nil))
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == true
+    return false
+  elsif index.between?(0,8) == false
+    return false
+  else
+    return true
+  end
+end
